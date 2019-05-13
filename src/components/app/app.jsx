63cbd36bdec6d4194ca4d064ba423e751a56f8ff
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-05-13 22:06:14 +0500

    add basic game functionality

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,23 +1,100 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Welcome from 'components/welcome/welcome.jsx';
+import ArtistGame, {propTypes as ArtistGamePropTypes} from 'components/artist-game/artist-game.jsx';
+import GenreGame, {propTypes as GenreGamePropTypes} from 'components/genre-game/genre-game.jsx';
 
 const propTypes = {
   gameTime: PropTypes.number,
-  errorCount: PropTypes.number
+  errorCount: PropTypes.number,
+  questions: PropTypes.arrayOf(
+      PropTypes.oneOfType([
+        PropTypes.shape(GenreGamePropTypes),
+        PropTypes.shape(ArtistGamePropTypes)
+      ])
+  )
 };
 
-const App = (props) => {
-  const {
-    gameTime,
-    errorCount
-  } = props;
-
-  return <Welcome
-    time={gameTime}
-    errorCount={errorCount}
-  />;
+const Type = {
+  ARTIST: `artist`,
+  GENRE: `genre`,
 };
+
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gameType: null,
+      unAnsweredQuestionsCount: null,
+      genre: null,
+      artist: null,
+    };
+
+    this.onGameStart = this.onGameStart.bind(this);
+    this.onAnswer = this.onAnswer.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      unAnsweredQuestionsCount: this.props.questions.length,
+    });
+  }
+
+  onGameStart() {
+    this.setState({
+      gameType: this.state.gameType === Type.GENRE ? Type.ARTIST : Type.GENRE,
+    });
+  }
+
+  onAnswer({id, answer}) {
+    this.setState({
+      gameType: this.state.gameType === Type.GENRE ? Type.ARTIST : Type.GENRE,
+      [id]: answer,
+      unAnsweredQuestionsCount: this.state.unAnsweredQuestionsCount ? this.state.unAnsweredQuestionsCount - 1 : 0,
+    });
+  }
+
+  render() {
+    const {
+      gameTime,
+      errorCount,
+      questions,
+    } = this.props;
+
+    const gameInfo = this.state.gameType && questions.find((game) => game.type === this.state.gameType);
+
+    return (
+      <>
+      {
+        (!this.state.unAnsweredQuestionsCount || !this.state.gameType) && (
+          <Welcome
+            time={gameTime}
+            errorCount={errorCount}
+            onClick={this.onGameStart}
+          />
+        )
+      }
+      {
+        (this.state.gameType === `artist` && gameInfo && this.state.unAnsweredQuestionsCount) && (
+          <ArtistGame
+            {...gameInfo}
+            onClick={this.onAnswer}
+          />
+        )
+      }
+      {
+        (this.state.gameType === `genre` && gameInfo && this.state.unAnsweredQuestionsCount) && (
+          <GenreGame
+            {...gameInfo}
+            onClick={this.onAnswer}
+          />
+        )
+      }
+      </>
+    );
+  }
+}
 
 App.propTypes = propTypes;
 
--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -7,10 +7,40 @@ const gameSettings = {
   errorCount: 3,
 };
 
+const questions = [
+  {
+    id: `a`,
+    type: `genre`,
+    genre: `rock`,
+    answers: [
+      {
+        id: `a`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      },
+    ],
+  },
+  {
+    id: `b`,
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `path.mp3`,
+    },
+    answers: [
+      {
+        id: `b`,
+        picture: `https://static.tvtropes.org/pmwiki/pub/images/jon_snow_s7.png`,
+        artist: `John Snow`,
+      },
+    ],
+  },
+];
+
 describe(`App component`, () => {
   it(`should render correctly`, () => {
     const tree = renderer
-      .create(<App {...gameSettings}/>)
+      .create(<App {...gameSettings} questions={questions}/>)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
