777893bd6b4c7e8d63ade0b3e9675e6dd482c7b6
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-05-13 22:06:59 +0500

    import questions mocks

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import App from 'components/app/app.jsx';
 
 import settings from 'mocks/settings';
+import questions from 'mocks/questions';
 
 ReactDOM.render(
     <App
       {...settings}
+      questions={questions}
     />,
     document.querySelector(`.main`)
 );
